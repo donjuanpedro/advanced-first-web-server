df553516e95ee2297772a37eb2a08ea04a617c79
Walker Lyle <walker@Walkers-MacBook-Air.local>
2018-08-01 20:33:51 -0500

    all requests working

---
+++ index.js
@@ -0,0 +1,37 @@
+let express = require("express");
+let users = require("./state").users;
+
+const app = express();
+
+
+//hey express, anytime you recieve a request,
+//use this function to decide what to repond with
+app.use(function(req,res,next){
+    if(req.path == "/users" && req.method == "GET") {
+        return res.send(users);
+    }
+    if(req.path == "/users/1" && req.method == "GET") {
+        return res.send(users[0]);
+    }
+    if(req.path == "/users" && req.method == "POST") {
+        users.push({_id: "6", name:"walker", occupation:"web developer", avatar:"#"});
+        return res.send(users);
+    }
+    if(req.path == "/users/1" && req.method == "PUT") {
+        (users[0].name = "John" );
+        return res.send(users);
+    }
+    if(req.path == "/users/1" && req.method == "DELETE") {
+        users.pop();
+        return res.send("Deleted.");
+    }
+   return res.send(users);
+ });
+ 
+
+app.listen(3002, (err) => {
+    if (err) {
+        return console.log("Error", err);
+    }
+    console.log("Web server is now living in apartment 3002");
+});
